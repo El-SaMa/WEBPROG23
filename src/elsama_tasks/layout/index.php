3643d4522142b4530412820e19b53c2690f86eb3
Sa7mas <57360146+se7mas@users.noreply.github.com>
2023-02-01 09:43:46 +0200

    .
    .

---
+++ src/elsama_tasks/layout/index.php
@@ -0,0 +1,12 @@
+<?php include "header.php"?>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor corporis repellat nesciunt velit voluptate, quae rem optio, provident facere doloribus id molestiae a deleniti non modi aperiam officia! Harum, odit.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente velit a magnam nostrum consectetur molestias. Ducimus quas neque maxime, repellendus saepe labore numquam unde vitae molestiae consequatur totam quam ab?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit modi necessitatibus recusandae possimus! Labore, dolores blanditiis corporis aliquid a delectus, veritatis aut praesentium alias debitis officia neque obcaecati ad perspiciatis?
+</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem eveniet sapiente, culpa illo perspiciatis vero natus labore at consequuntur, optio ex laborum vel incidunt temporibus facere iste magni qui pariatur. 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam et quidem nobis labore odio quos adipisci facilis iure. Maiores dignissimos veritatis porro mollitia numquam ullam doloribus vero doloremque commodi accusamus!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nihil laboriosam beatae impedit excepturi adipisci consequuntur similique, sint, vero assumenda labore harum consectetur quo quibusdam rerum autem culpa veniam modi.
+</p>
+
+<?php include"footer.php"?>
