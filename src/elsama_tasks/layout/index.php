5f22339bdc4cccd77dabe8330e19eff9d3f75037
Sa7mas <57360146+se7mas@users.noreply.github.com>
2023-02-01 10:21:19 +0200

    .

--- src/elsama_tasks/layout/index.php
+++ src/elsama_tasks/layout/index.php
@@ -1,5 +1,5 @@
 <?php include "header.php"?>
-
+<div class="col-sm-9">
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor corporis repellat nesciunt velit voluptate, quae rem optio, provident facere doloribus id molestiae a deleniti non modi aperiam officia! Harum, odit.
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente velit a magnam nostrum consectetur molestias. Ducimus quas neque maxime, repellendus saepe labore numquam unde vitae molestiae consequatur totam quam ab?
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit modi necessitatibus recusandae possimus! Labore, dolores blanditiis corporis aliquid a delectus, veritatis aut praesentium alias debitis officia neque obcaecati ad perspiciatis?
@@ -9,4 +9,28 @@
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nihil laboriosam beatae impedit excepturi adipisci consequuntur similique, sint, vero assumenda labore harum consectetur quo quibusdam rerum autem culpa veniam modi.
 </p>
 
+      <h2>What is bootstrap ? </h2> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/440px-Bootstrap_logo.svg.png" align="right" height="150" width="150">
+      <p>Bootstrap is a free and open-source CSS framework directed at responsive,
+         mobile-first front-end web development. It contains CSS- and (optionally) 
+         JavaScript-based design templates for typography, forms, buttons, navigation
+          and other interface components.
+
+        <p>Bootstrap is the sixth-most-starred project on GitHub, with more than 135,000 stars, behind freeCodeCamp (almost 307,000 stars) and marginally behind Vue.js framework.[2] According to Alexa Rank, Bootstrap getbootstrap.com
+         is in the top-2000 in US while vuejs.org is in top-7000 in US.[3]</p></p>
+      <br>
+      <h2>Bootstrap 4</h2>
+      <p>
+
+        Bootstrap 4
+        Mark Otto announced Bootstrap 4 on October 29, 2014.
+        The first alpha version of Bootstrap 4 was released on August 19, 2015.[10]
+         The first beta version was released on 10 August 2017.[11] 
+         Mark suspended work on Bootstrap 3 on September 6, 2016, to free up 
+         time to work on Bootstrap 4.
+         Bootstrap 4 was finalized on January 18, 2018.
+      </p>
+      
+      <p>Article Src: https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework)</p>
+
+    </div>
 <?php include"footer.php"?>
