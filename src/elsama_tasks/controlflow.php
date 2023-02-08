aa684f09efdabdf5964104f2ef6d817b36f5b923
El-SaMa <113467305+El-SaMa@users.noreply.github.com>
2023-02-08 23:26:38 +0200

    Update controlflow.php

--- src/elsama_tasks/controlflow.php
+++ src/elsama_tasks/controlflow.php
@@ -14,7 +14,67 @@ if($month_num == 8){
 <h1>Task 2</h1>
 <?php 
 $color="Red";
+if ($color == "Red") 
+
+    echo "The color is indeed Red";
+
+else 
+
+    echo "The color is not Red";
+
+
 ?>
+
+<h1>Task 3</h1>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+Enter your grade total: <input type="text" name="total"><br>
+<input type="submit" name="submit">
+</form>
+<?php 
+/*
+Excellent: >80; 
+Great >70 & less than 80;
+Good >60 & less than 70; 
+Pass >50 & less than 60
+Fail <50
+*/
+$grade =null;
+$total = null;
+
+if (isset($_POST["submit"])){
+$total = $_POST['total'];
+}
+if ($total >= 80)
+{
+    $grade = "Excellent";
+    echo "Your grade is $grade";
+}
+else if ($total >= 70 && $total < 80)
+{
+    $grade = "Great";
+    echo "Your grade is $grade";
+}
+else if ($total >= 60 && $total < 70)
+{
+    $grade = "Good";
+    echo "Your grade is $grade";
+}
+else if ($total >= 50 && $total < 60)
+{
+    $grade = "Pass";
+    echo "Your grade is $grade";
+}
+else
+{
+    $grade = "Fail";
+    echo "Your grade is $grade";
+}
+
+?> 
+
+<h1>Task 4</h1>
+
+
 
   
 <?php include "footer.php";?>
