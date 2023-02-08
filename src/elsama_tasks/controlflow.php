9d6442a5078122ade8776a77fc352da7d2af4c0c
El-SaMa <113467305+El-SaMa@users.noreply.github.com>
2023-02-09 00:03:02 +0200

    Update controlflow.php

--- src/elsama_tasks/controlflow.php
+++ src/elsama_tasks/controlflow.php
@@ -1,24 +1,23 @@
-<?php include"header.php"?>
+<?php include "header.php" ?>
 
 <h1>Task 1</h1>
-<?php 
+<?php
 $month_num = date('m');
 $month = date('F');
-if($month_num == 8){
-   echo "It's August, so it's still holiday.";
+if ($month_num == 8) {
+    echo "It's August, so it's still holiday.";
 } else {
-   echo "Not August, this is $month, so I don't have any holidays";
+    echo "Not August, this is $month, so I don't have any holidays";
 }
 ?>
 
 <h1>Task 2</h1>
-<?php 
-$color="Red";
-if ($color == "Red") 
+<?php
+$color = "Red";
+if ($color == "Red")
 
     echo "The color is indeed Red";
-
-else 
+else
 
     echo "The color is not Red";
 
@@ -27,10 +26,10 @@ else
 
 <h1>Task 3</h1>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-Enter your grade total: <input type="text" name="total" required><br>
-<input type="submit" name="submit">
+    Enter your grade total: <input type="text" name="total" required><br>
+    <input type="submit" name="submit">
 </form>
-<?php 
+<?php
 /*
 Excellent: >80; 
 Great >70 & less than 80;
@@ -38,61 +37,52 @@ Good >60 & less than 70;
 Pass >50 & less than 60
 Fail <50
 */
-$grade =null;
+$grade = null;
 $total = null;
 
-if (isset($_POST["submit"])){
-$total = $_POST['total'];
+if (isset($_POST["submit"])) {
+    $total = $_POST['total'];
 }
-if ($total >= 80)
-{
+if ($total >= 80) {
     $grade = "Excellent";
     echo "Your grade is $grade";
-}
-else if ($total >= 70 && $total < 80)
-{
+} else if ($total >= 70 && $total < 80) {
     $grade = "Great";
     echo "Your grade is $grade";
-}
-else if ($total >= 60 && $total < 70)
-{
+} else if ($total >= 60 && $total < 70) {
     $grade = "Good";
     echo "Your grade is $grade";
-}
-else if ($total >= 50 && $total < 60)
-{
+} else if ($total >= 50 && $total < 60) {
     $grade = "Pass";
     echo "Your grade is $grade";
-}
-else
-{
+} else {
     $grade = "Fail";
     echo "Your grade is $grade";
 }
 
-?> 
+?>
 
 <h1>Task 4</h1>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-Name: <input type="text" name="name" required><br>
-Age: <input type="text" name="age" required><br>
-<input type="submit" name="submit">
+    Name: <input type="text" name="name" required><br>
+    Age: <input type="text" name="age" required><br>
+    <input type="submit" name="submit">
 </form>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = null;
     $age = null;
-  if (isset($_POST["submit"])){
-  
-  $name = $_POST['name'];
-  $age = $_POST['age'];
-}
-  if (!empty($age || $name) && age >= 18) {
-    echo "Thank you $name, you are eligible to vote.";
-  } else if (!empty($age || $name) && age < 18){
-    echo "Thank you $name, you are not eligible to vote.";
-  } 
+    if (isset($_POST["submit"])) {
+
+        $name = $_POST['name'];
+        $age = $_POST['age'];
+    }
+    if (!empty($age || $name) && age >= 18) {
+        echo "Thank you $name, you are eligible to vote.";
+    } else if (!empty($age || $name) && age < 18) {
+        echo "Thank you $name, you are not eligible to vote.";
+    }
 }
 ?>
 
@@ -102,12 +92,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <?php
 $browser = $_SERVER['HTTP_USER_AGENT'];
-$browser = get_browser();
+
+switch ($browser) {
+    case (strpos($browser, 'Edge') == true):
+        echo "You're currently using Edge";
+        break;
+
+    case (strpos($browser, 'Chrome') == true):
+        echo "You're currently using Google Chrome";
+        break;
+
+    case (strpos($browser, 'Firefox') == true):
+        echo "You're currently using Mozilla Firefox";
+        break;
 
 
-echo "Your browser information: </br>".$_SERVER['HTTP_USER_AGENT'];
+}
 
 ?>
 
-  
-<?php include "footer.php";?>
+
+<?php include "footer.php"; ?>
